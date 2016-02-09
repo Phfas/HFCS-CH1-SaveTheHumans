fa435f9d3d8b3636b41f0db121932c38f3d3de56
phfas <phfas@outlook.com>
2016-02-08 22:07:42 -0700

    added startbutton_Click created addEnemy() method

--- Save the Humans/MainWindow.xaml.cs
+++ Save the Humans/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Save_the_Humans
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddEnemy();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), 
+                random.Next((int)playArea.ActualHeight - 100), "Canvas.Top");
+            playArea.Children.Add(enemy);
+
+        }
+
+        private void AnimateEnemy(ContentControl enemy, int v1, double v2, string v3)
+        {
+            throw new NotImplementedException();
         }
     }
 }
